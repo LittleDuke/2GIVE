5069848f1fceab3a130df5f9a8cb2ee109be968a
Jay Carlson <nop@nop.com>
2016-05-03 14:43:55 -0400

    Round two of modernization mods

--- src/serialize.h
+++ src/serialize.h
@@ -824,6 +824,7 @@ public:
             vch.insert(it, first, last);
     }
 
+#ifndef SERIALIZE_H_HAS_DUPLICATE_DECLARATION    
     void insert(iterator it, std::vector<char>::const_iterator first, std::vector<char>::const_iterator last)
     {
         assert(last - first >= 0);
@@ -836,7 +837,8 @@ public:
         else
             vch.insert(it, first, last);
     }
-
+#endif
+    
 #if !defined(_MSC_VER) || _MSC_VER >= 1300
     void insert(iterator it, const char* first, const char* last)
     {
