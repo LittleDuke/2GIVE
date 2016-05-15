4f9a093b67ecc0a8f007bd7aa8546de17fe79b32
LittleDuke <dduccini@gmail.com>
2016-05-15 17:48:43 -0500

    + Updated About dialog to give a shout out to our friends at http://peacetimepropaganda.com + Updated Version to RC4

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("2GiveCoin");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-RC4-DEV"
+#define CLIENT_VERSION_SUFFIX   "-RC4"
 #define BUILD_DATE __DATE__ ", " __TIME__
 
 // The following part of the code determines the CLIENT_BUILD variable.
