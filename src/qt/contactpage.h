72ea83fc18fe00421eab15dd8064d6380eead24c
LittleDuke <dduccini@gmail.com>
2016-07-29 07:26:49 -0500

    Initial commit

---
+++ src/qt/contactpage.h
@@ -0,0 +1,92 @@
+#ifndef CONTACTPAGE_H
+#define CONTACTPAGE_H
+
+#include "contactdatamanager.h"
+
+#include <QDialog>
+
+namespace Ui {
+    class ContactPage;
+}
+
+class ContactTableModel;
+class OptionsModel;
+
+QT_BEGIN_NAMESPACE
+class QTableView;
+class QItemSelection;
+class QSortFilterProxyModel;
+class QMenu;
+class QModelIndex;
+QT_END_NAMESPACE
+
+/** Widget that shows a list of sending or receiving addresses.
+  */
+class ContactPage : public QDialog
+{
+    Q_OBJECT
+
+public:
+    enum Tabs {
+        SendingTab = 0,
+        ReceivingTab = 1
+    };
+
+    enum Mode {
+        ForSending, /**< Open address book to pick address for sending */
+        ForEditing  /**< Open address book for editing */
+    };
+
+    explicit ContactPage(Mode mode, Tabs tab, QWidget *parent = 0);
+    ~ContactPage();
+
+    void setModel(ContactTableModel *model);
+    void setOptionsModel(OptionsModel *optionsModel);
+    const QString &getReturnValue() const { return returnValue; }
+
+public slots:
+    void done(int retval);
+    void exportClicked();
+
+private:
+    Ui::ContactPage *ui;
+    ContactTableModel *model;
+    OptionsModel *optionsModel;
+    Mode mode;
+    Tabs tab;
+    QString returnValue;
+    QSortFilterProxyModel *proxyModel;
+    QMenu *contextMenu;
+    QAction *deleteAction;
+    QString newAddressToSelect;
+    ContactDataManager ccdb;
+
+
+private slots:
+    void on_giveButton_clicked();
+    void on_editButton_clicked();
+    void on_deleteButton_clicked();
+    void on_newContactButton_clicked();
+    /** Copy address of currently selected address entry to clipboard */
+    void on_copyToClipboard_clicked();
+    void on_signMessage_clicked();
+    void on_verifyMessage_clicked();
+    void selectionChanged();
+    void on_showQRCode_clicked();
+    /** Spawn contextual menu (right mouse menu) for address book entry */
+    void contextualMenu(const QPoint &point);
+
+    /** Copy label of currently selected address entry to clipboard */
+    void onCopyLabelAction();
+    /** Edit currently selected address entry */
+    void onEditAction();
+
+    /** New entry/entries were added to address table */
+    void selectNewAddress(const QModelIndex &parent, int begin, int end);
+
+signals:
+    void signMessage(QString addr);
+    void verifyMessage(QString addr);
+};
+
+#endif // CONTACTPAGE_H
---
+++ src/qt/contacttablemodel.h
@@ -0,0 +1,95 @@
+#ifndef CONTACTTABLEMODEL_H
+#define CONTACTTABLEMODEL_H
+
+#include <QAbstractTableModel>
+#include <QStringList>
+
+#include "contactdatamanager.h"
+
+class ContactTablePriv;
+class CWallet;
+class WalletModel;
+
+/**
+   Qt model of the address book in the core. This allows views to access and modify the address book.
+ */
+class ContactTableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+public:    
+    explicit ContactTableModel(ContactDataManager ccdb, WalletModel *parent = 0);
+    ~ContactTableModel();
+
+    enum ColumnIndex {
+        Label = 0,
+        Address = 1,
+        Email = 2,
+        URL = 3
+    };
+
+    enum RoleIndex {
+        TypeRole = Qt::UserRole /**< Type of address (#Send or #Receive) */
+    };
+
+    /** Return status of edit/insert operation */
+    enum EditStatus {
+        OK,
+        INVALID_ADDRESS,   /**< Unparseable address */
+        DUPLICATE_ADDRESS,  /**< Address already in address book */
+        WALLET_UNLOCK_FAILURE, /**< Wallet could not be unlocked to create new receiving address */
+        KEY_GENERATION_FAILURE /**< Generating a new public key for a receiving address failed */
+    };
+
+    /** @name Methods overridden from QAbstractTableModel
+        @{*/
+    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    QVariant data(const QModelIndex &index, int role) const;
+    bool setData(const QModelIndex & index, const QVariant & value, int role);
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    QModelIndex index(int row, int column, const QModelIndex & parent) const;
+    bool removeRows(int row, int count, const QModelIndex & parent = QModelIndex());
+    Qt::ItemFlags flags(const QModelIndex & index) const;
+    /*@}*/
+
+    /* Add an address to the model.
+       Returns the added address on success, and an empty string otherwise.
+     */
+    QString addRow(const QString &label, const QString &address, const QString &email, const QString &url);
+
+    /* Look up label for address in address book, if not found return empty string.
+     */
+    QString labelForAddress(const QString &address) const;
+
+    /* Look up row index of an address in the model.
+       Return -1 if not found.
+     */
+    int lookupAddress(const QString &address) const;
+
+    EditStatus getEditStatus() const { return editStatus; }
+
+    ContactDataManager contactDataBase(void);
+
+private:
+    WalletModel *walletModel;
+    ContactDataManager ccdb;
+    ContactTablePriv *priv;
+    QStringList columns;
+    EditStatus editStatus;
+
+    /** Notify listeners that data changed. */
+    void emitDataChanged(int index);
+
+signals:
+    void defaultAddressChanged(const QString &address);
+
+public slots:
+    /* Update address list from core.
+     */
+    void updateEntry(const QString &address, const QString &label, const QString &email, const QString &url, int status);
+    void refreshContactTable(void);
+
+    friend class ContactTablePriv;
+};
+
+#endif // CONTACTTABLEMODEL_H
