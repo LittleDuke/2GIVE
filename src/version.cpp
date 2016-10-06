c7d246a12738d2611489b8599881c39d10f10774
LittleDuke <dduccini@gmail.com>
2016-10-06 10:39:29 -0500

    Updated splash screen and version to RC6

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("2GiveCoin");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-RC6-DEV"
+#define CLIENT_VERSION_SUFFIX   "-RC6"
 #define BUILD_DATE __DATE__ ", " __TIME__
 
 // The following part of the code determines the CLIENT_BUILD variable.
